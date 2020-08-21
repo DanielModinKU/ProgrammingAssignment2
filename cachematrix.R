33279435a1195e28b793d3137e8bdc35e8881091
Daniel Modin <danielmodin.md@gmail.com>
2020-08-21 11:45:01 +0200

    my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## creates a cache object for the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL 
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function () {x}
+    setinv <- function(inv) {inverse <<- inv}
+    getinv <- function() {inverse} 
+    object = list(set = set, get = get, setinv = setinv, getinv = getinv)
+    return(object)
 }
 
 
-## Write a short comment describing this function
-
+## calculates the inverse if no cache is cached or retrieves the cache if present
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinv()
+    if (!is.null(inverse)) {
+        print('get cached data')
+        return(inverse)                   #will return here and break out of function if cached is present 
+    }
+    data <- x$get() 
+    inverse <- solve(data,...)
+    x$setinv(inverse)
+    return(inverse)
 }
+
+n=100
+mat = matrix(sample(1:(n*n), n*n), nrow=n, ncol=) #create matrix with random numbers
+
+mat_special = makeCacheMatrix(mat) #make special matrix for use with cache
+
+
+#call nr 1, calculate inverse
+inverted = cacheSolve(mat_special) 
+
+#call nr 2, should retrieve cache instead and print "get cached data" 
+inverted2 = cacheSolve(mat_special)
+
+#check if identical 
+identical(inverted,inverted2)
+
+
